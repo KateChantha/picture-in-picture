0219f77f22d7435897c90319f05f3ebd953bb41f
Kato <kubkate@gmail.com>
2020-08-25 19:11:35 -0700

    add button evenListener to start picture in picture

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const vidoeElement = document.getElementById('video');
+const videoElement = document.getElementById('video');
 const button = document.getElementById('button');
 
 // sreen capture api
@@ -8,15 +8,25 @@ async function selectMedia() {
     // assign mediaStream to whichever screen/window that user selected to share
     const mediaStream = await navigator.mediaDevices.getDisplayMedia();
     // pass mediaStream into video object as its source/value of object
-    vidoeElement.srcObject = mediaStream;
+    videoElement.srcObject = mediaStream;
     // when video has loaded metadata , call a fuction to play a video
-    vidoeElement.onloadeddata = () => {
-      vidoeElement.play();
+    videoElement.onloadeddata = () => {
+      videoElement.play();
     }
   } catch (error) {
     console.log('whoops, error in select media', error)
   }
 }
+
+button.addEventListener('click', async () => {
+  console.log("CLICK!!!!")
+  // Disable Button
+  button.disabled = true;
+  // Start Picture in Picture
+  await videoElement.requestPictureInPicture();
+  // Reset BUtton after successfully requestPictureInPicture, otherwise button will remain disabled
+  button.disabled = false;
+})
 
 // On Load
 selectMedia();
