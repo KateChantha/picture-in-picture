307a0fc570f352ea1dd4aa665ee2546e35d47c45
Kato <kubkate@gmail.com>
2020-08-28 18:03:52 -0700

    Add logic to show and hide play/stop button

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 const videoElement = document.getElementById('video');
-const button = document.getElementById('button');
+const buttonSelect = document.getElementById('button-select');
+const buttonShare = document.getElementById('button-share');
 
+buttonShare.style.display = 'none';
+let mediaStream;
 // sreen capture api
 // Prompt to select media stream, pass to video element, then play.
 async function selectMedia() {
   try {
     // assign mediaStream to whichever screen/window that user selected to share
-    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    mediaStream = await navigator.mediaDevices.getDisplayMedia();
     // pass mediaStream into video object as its source/value of object
     videoElement.srcObject = mediaStream;
     // when video has loaded metadata , call a fuction to play a video
@@ -15,18 +18,46 @@ async function selectMedia() {
     }
   } catch (error) {
     console.log('whoops, error in select media', error)
+    buttonShare.style.display = 'none';
   }
 }
 
-button.addEventListener('click', async () => {
-  console.log("CLICK!!!!")
-  // Disable Button
-  button.disabled = true;
-  // Start Picture in Picture
-  await videoElement.requestPictureInPicture();
-  // Reset BUtton after successfully requestPictureInPicture, otherwise button will remain disabled
-  button.disabled = false;
-})
+buttonSelect.addEventListener('click', () => {
+  selectMedia();
+  buttonShare.innerText = 'Play PIP Mode';
+  buttonShare.style.display = 'block';
+});
 
-// On Load
-selectMedia();
+let mediaStreamTrack;
+const stopMediaStreamOn = () => {
+  mediaStreamTrack = mediaStream.getVideoTracks();
+  // mediaStreamTrack && mediaStreamTrack[0].stop();
+  // mediaStreamTrack[0].stop();
+  mediaStreamTrack.forEach(media => media.stop());
+};
+
+
+let isShared = true;
+buttonShare.addEventListener('click', async () => {
+  mediaStreamTrack = mediaStream.getVideoTracks();
+  console.log('mediaStreamTrack', mediaStreamTrack)
+  // Disable Button
+  buttonShare.disabled = true;
+  // Start Picture in Picture
+  if (isShared) {
+    await videoElement.requestPictureInPicture();
+    buttonShare.innerText = 'Stop PIP Mode';
+    isShared = false;
+  } else {
+    console.log('before exit', isShared)
+    await document.exitPictureInPicture();
+    isShared = true;
+    console.log('after exit', isShared)
+    stopMediaStreamOn();
+    buttonShare.style.display = 'none';
+  }
+
+  // Reset BUtton after successfully requestPictureInPicture, otherwise button will remain disabled
+  buttonShare.disabled = false;
+});
+
